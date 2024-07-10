f3adb36646ad64437b4673096aae8789374c7b6e
Beanspoon <peter.s.kubiak@bath.edu>
2024-07-10 16:21:19 +0100

    Draw a rhombus

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
-use bevy::prelude::*;
+use bevy::{
+    prelude::*,
+    sprite::{MaterialMesh2dBundle, Mesh2dHandle},
+};
 
 fn main() {
     App::new()
@@ -7,7 +10,12 @@ fn main() {
         .run();
 }
 
-fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
+fn setup(
+    mut commands: Commands,
+    asset_server: Res<AssetServer>,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<ColorMaterial>>,
+) {
     let font = asset_server.load("fonts/ARLRDBD.TTF");
 
     commands.spawn(Camera2dBundle::default());
@@ -21,6 +29,14 @@ fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
                 ..default()
             },
         ),
+        ..default()
+    });
+
+    let rhombus = Rhombus::new(100., 100.);
+    let rhombus_handle = Mesh2dHandle(meshes.add(rhombus));
+    commands.spawn(MaterialMesh2dBundle {
+        mesh: rhombus_handle,
+        material: materials.add(Color::hsl(0.0, 0.3, 1.)),
         ..default()
     });
 }
