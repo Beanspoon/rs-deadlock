42ce3a72c368a9a4a972012a3b174db3f9528b01
Beanspoon <peter.s.kubiak@bath.edu>
2024-07-10 17:16:12 +0100

    Draw a grid converting isometric to cartesian co-ordinates (first attempt)

--- src/main.rs
+++ src/main.rs
@@ -27,12 +27,19 @@ fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
 }
 
 fn draw_grid(mut gizmos: Gizmos) {
-    gizmos.primitive_2d(
-        &Rhombus {
-            half_diagonals: Vec2 { x: 100.0, y: 75.0 },
-        },
-        Vec2::ZERO,
-        0.0,
-        WHITE_SMOKE,
-    );
+    for index in 0..36 {
+        let iso_x = (index % 6) as f32 * 100.0;
+        let iso_y = (index / 6) as f32 * 100.0;
+
+        let car_x = (iso_x - iso_y) / 1.5;
+        let car_y = iso_x / 3.0 + iso_y / 1.5;
+        gizmos.primitive_2d(
+            &Rhombus {
+                half_diagonals: Vec2 { x: 100.0, y: 75.0 },
+            },
+            Vec2 { x: car_x, y: car_y },
+            0.0,
+            WHITE_SMOKE,
+        );
+    }
 }
