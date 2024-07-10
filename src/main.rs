02f5ec7db108bf61a3ed3d18d60266146934c560
Beanspoon <peter.s.kubiak@bath.edu>
2024-07-10 16:04:00 +0100

    Display some text in the window

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+use bevy::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_systems(Startup, setup)
+        .run();
+}
+
+fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
+    let font = asset_server.load("fonts/ARLRDBD.TTF");
+
+    commands.spawn(Camera2dBundle::default());
+
+    commands.spawn(Text2dBundle {
+        text: Text::from_section(
+            "Hello",
+            TextStyle {
+                font: font.clone(),
+                font_size: 60.,
+                ..default()
+            },
+        ),
+        ..default()
+    });
 }
